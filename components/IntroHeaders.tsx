dd61e5a3b5b66795641f0689dd2495f69ca8cc7a
Hossam Mohamed <hossammohamed9784@gmail.com>
2024-08-31 13:22:51 +0100

    Some improvements to Intro section

--- components/IntroHeaders.tsx
+++ components/IntroHeaders.tsx
@@ -5,14 +5,14 @@ const IntroHeaders = () => {
   return (
     <div className="flex justify-center relative my-20 z-10">
       <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-        <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-100">
+        <p className="uppercase tracking-widest text-[10px] md:text-xs text-center text-blue-100">
         Innovating Web Solutions from Frontend to Backend
         </p>
         <TextGenerateEffect
           words="Turning Concepts into Scalable, High-Performance Applications"
-          className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          className="text-center"
         />
-        <p>Hi, I&apos;m Hossam, a full-stack Developer with passion for problem-solving</p>
+        <p className="text-center mt-3 md:tracking-wider text-sm md:text-lg">Hi, I&apos;m Hossam, a full-stack developer with passion for problem-solving</p>
       </div>
     </div>
   );
